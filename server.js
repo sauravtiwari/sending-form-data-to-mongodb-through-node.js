1ea46646edfb1c13ae595d163e2fd41dc9b7f3a7
sauravtiwari <sauravtiwari34@gmail.com>
2017-11-09 17:08:51 +0530

    sending a form data in mongodb through node js and also extracting it from database

---
+++ server.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var app = express();
+
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+app.use(express.static('public'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+mongoose.connect('mongodb://localhost/connection',function(err){
+  if(err){
+    console.log(`error is ${err}`);
+  }
+  else
+  {
+    console.log('connected to the database');
+  }
+});
+
+var Schema = new mongoose.Schema({
+	_id    : String,
+	name: String,
+	age   : Number
+});
+
+var user = mongoose.model('emp', Schema);
+
+app.post('/new', function(req, res){
+	new user({
+		_id    : req.body.email,
+		name: req.body.name,
+		age   : req.body.age
+	}).save(function(err, doc){
+		if(err) res.json(err);
+		else    res.send('Successfully inserted!');
+	});
+});
+
+app.get('/view', function(req, res){
+  user.find({});
+});
+
+app.listen(3000,function () {
+   console.log('server is running on port 3000');
+});
